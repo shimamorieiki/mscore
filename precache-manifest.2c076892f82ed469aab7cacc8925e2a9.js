44d83b4d4d4c65849dde6cafa126943cc2b194b7
TKparadise <paradiseoftk@gmail.com>
2020-09-12 19:30:33 +0900

    Updates

--- precache-manifest.2c076892f82ed469aab7cacc8925e2a9.js
+++ precache-manifest.2c076892f82ed469aab7cacc8925e2a9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "886f0194ab348d5bc228f17382499751",
+    "revision": "f199cbfa5eedab58517dde6cdf464002",
     "url": "/mscore/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mscore/static/js/2.cf852713.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "413812d67df6ed2b96ff",
-    "url": "/mscore/static/js/main.8b844d6e.chunk.js"
+    "revision": "3b83ad5eb4afc70cd18a",
+    "url": "/mscore/static/js/main.9b61743e.chunk.js"
   },
   {
     "revision": "012f4eeba2a8d79084aa",
